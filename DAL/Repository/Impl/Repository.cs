5d07e818eabbb90d5033736dd80eef38b4e0e6fb
ferdinandsen <Jacobferdinandsen@gmail.com>
2014-09-11 10:53:07 +0200

    ....

--- DAL/Repository/Impl/Repository.cs
+++ DAL/Repository/Impl/Repository.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace DAL.Repository.Impl
 {
-    class Repository
+    internal class Repository : IRepository
     {
+        public List<Album> GetAllAlbums()
+        {
+            using (DBConnection db = new DBConnection())
+            {
+                return db.Albums.ToList();
+            }
+        }
+
+        public List<Artist> GetArtistByAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Details(Album album)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MusicStore/Controllers/AlbumController.cs
+++ MusicStore/Controllers/AlbumController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DAL;
+using DAL.Repository;
+using DAL.Repository.Impl;
 
 namespace MusicStore.Controllers
 {
     public class AlbumController : Controller
     {
+        IRepository rep = new Repository();
         // GET: Album
         public ActionResult Index()
         {
